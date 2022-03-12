dca37388c3ceee9a5a36c110398837b34c094f7c
Ricardo Antonio <ricardoantoniomx@gmail.com>
2022-03-11 22:29:12 -0600

    Bio and Blog page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,59 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 
 const Layout = ({ pageTitle, children, location }) => {
   const rootPath = `${__PATH_PREFIX__}/`;
   const isNotRootPath = location.pathname !== rootPath;
 
-  return <div>{isNotRootPath && <h1>{pageTitle}</h1>}</div>;
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  const linkPages = [
+    {
+      name: "Inicio",
+      link: "/",
+    },
+    {
+      name: "Blog",
+      link: "/blog",
+    },
+  ];
+
+  return (
+    <div>
+      <title>
+        {data.site.siteMetadata.title} | {pageTitle}
+      </title>
+
+      <header>
+        <Link to="/">
+        <span>{data.site.siteMetadata.title}</span></Link>
+        <nav>
+          <ul>
+            {linkPages.map((page) => (
+              <li>
+                <Link to={page.link} activeStyle={{ color: "red" }}>
+                  {page.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        {isNotRootPath && <h1>{pageTitle}</h1>}
+        {children}
+      </main>
+    </div>
+  );
 };
 
 export default Layout;
---
+++ src/pages/blog/index.js
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import Layout from '../../components/layout'
+
+const BlogPage = ({location}) => {
+  return <Layout pageTitle="Blog" location={location}></Layout>
+}
+
+export default BlogPage
