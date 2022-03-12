47da13c14bdb9405bf2198dd04446bafc2f89e03
Ricardo Antonio <ricardoantoniomx@gmail.com>
2022-03-11 23:37:05 -0600

    images in post

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,25 +1,43 @@
 import * as React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
-import { mdx } from "@mdx-js/react";
 
 const PostPage = ({ data, location }) => {
-  return <Layout pageTitle={data.mdx.frontmatter.title} location={location}>
-<MDXRenderer>{data.mdx.body}</MDXRenderer>
-  </Layout>;
+  const heroImage = getImage(data.mdx.frontmatter.hero_image);
+
+  return (
+    <Layout pageTitle={data.mdx.frontmatter.title} location={location}>
+      <p>Posted: {data.mdx.frontmatter.date}</p>
+      {heroImage && (
+        <GatsbyImage
+          image={heroImage}
+          alt={data.mdx.frontmatter.hero_image_alt}
+        />
+      )}
+
+      <MDXRenderer>{data.mdx.body}</MDXRenderer>
+    </Layout>
+  );
 };
 
 export const query = graphql`
-query ($id: String) {
-  mdx(id: {eq: $id}) {
-    frontmatter {
-      title
-      date
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date
+        hero_image_alt
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+      body
     }
-    body
   }
-}
-`
+`;
 
-export default PostPage
+export default PostPage;
