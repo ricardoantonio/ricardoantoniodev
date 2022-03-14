63e48db3d74aca3f02afe95b88993b03aa997e0f
Ricardo Antonio <ricardoantoniomx@gmail.com>
2022-03-14 01:20:28 -0600

    change hero_image for featured_image

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -5,7 +5,7 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
 
 const PostPage = ({ data, location }) => {
-  const heroImage = getImage(data.mdx.frontmatter.hero_image);
+  const heroImage = getImage(data.mdx.frontmatter.featured_image);
 
   return (
     <Layout pageTitle={data.mdx.frontmatter.title} location={location}>
@@ -18,7 +18,7 @@ const PostPage = ({ data, location }) => {
         {heroImage && (
           <GatsbyImage
             image={heroImage}
-            alt={data.mdx.frontmatter.hero_image_alt}
+            alt={data.mdx.frontmatter.featured_image_alt}
             style={{marginBottom: '3.2rem'}}
           />
         )}
@@ -28,15 +28,15 @@ const PostPage = ({ data, location }) => {
     </Layout>
   );
 };
-
+console.log('holamundo')
 export const query = graphql`
   query ($id: String) {
     mdx(id: { eq: $id }) {
       frontmatter {
         title
         date(formatString: "dddd, D [de] MMMM [de] YYYY", locale: "es")
-        hero_image_alt
-        hero_image {
+        featured_image_alt
+        featured_image {
           childImageSharp {
             gatsbyImageData
           }
